c090398cb4a6f33eb48e0d5ca497fa60d8d5af6f
Ufych <69392227+Ufych@users.noreply.github.com>
2020-08-08 17:07:28 +0200

    initial commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$('#toolbar .hamburger').on('click', function() {
+  $('#toolbar .hamburger').parent().toggleClass('open');
+});
+
+$('#toolbar2 .hamburger2').on('click', function() {
+  $('#toolbar2 .hamburger2').parent().toggleClass('open2');
+});
+
+$('#toolbar4 .hamburger4').on('click', function() {
+  $('#toolbar4 .hamburger4').parent().toggleClass('close4');
+});
